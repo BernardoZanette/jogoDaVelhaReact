8f6a39168357bee035ca331ff92cafe3d8f783eb
Bernardo Zanette <bernardo.280706@bento.local>
2024-08-26 15:32:51 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import Jogo from './Jogo.jsx'
+import './App.css'
+
+function App() {
+  const [quadrados, setQuadrados] = useState(Array(9).fill(null));
+  const [turno, setTurno] = useState('X');
+  const [vitoria, setVitoria] = useState(0);
+
+  const combosVitoria = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+  ]
+
+  const handleJogar = (index) => {
+    if (quadrados[index] !== null) return;
+    const novoVetor = [...quadrados];
+    novoVetor[index] = turno;
+    setQuadrados(novoVetor);
+    setTurno(turno === "X" ? "O" : "X");
+    combosVitoria.forEach(combo => {
+      const primeiro = novoVetor[combo[0]] 
+      const segundo = novoVetor[combo[1]] 
+      const terceiro = novoVetor[combo[2]]
+      if (primeiro !== null && primeiro == segundo && segundo == terceiro) {
+        setVitoria(1)
+        console.log(`${turno} venceu!! :)`)
+      }
+    });
+  };
+  return (
+    <div>
+      <Jogo quadrados={quadrados} clique={handleJogar}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Jogo.jsx
@@ -0,0 +1,20 @@
+import './Jogo.css'
+import Quadrado from './Quadrado.jsx'
+
+const Jogo = ({quadrados, clique}) => {
+
+  return (
+    <div id='container'>
+      <Quadrado valor={quadrados[0]} onClick={() => clique(0)}/>
+      <Quadrado valor={quadrados[1]} onClick={() => clique(1)}/>
+      <Quadrado valor={quadrados[2]} onClick={() => clique(2)}/>
+      <Quadrado valor={quadrados[3]} onClick={() => clique(3)}/>
+      <Quadrado valor={quadrados[4]} onClick={() => clique(4)}/>
+      <Quadrado valor={quadrados[5]} onClick={() => clique(5)}/>
+      <Quadrado valor={quadrados[6]} onClick={() => clique(6)}/>
+      <Quadrado valor={quadrados[7]} onClick={() => clique(7)}/>
+      <Quadrado valor={quadrados[8]} onClick={() => clique(8)}/>
+    </div>  
+  );
+};
+export default Jogo;
